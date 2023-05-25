b553faacfc1db22edb72979474fc284141ef93c8
Andreas Krüger <ak@patientsky.com>
2023-05-25 12:26:21 +0200

    Delete route53_healtcheck_ips.sh

--- route53_healtcheck_ips.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq '.prefixes[] | select(.service == "ROUTE53_HEALTHCHECKS") | .ip_prefix' | tr -d \" > aws-healthcheck-ips.txt
